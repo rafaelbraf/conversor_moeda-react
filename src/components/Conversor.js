a7e7f8edfff9e53c8c2d8152a7123951e8594497
rafabrunof <rafabrunofelix@hotmail.com>
2019-11-10 21:20:39 -0300

    conversor de moeda

---
+++ src/components/Conversor.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import './Conversor.css';
+
+// import { Container } from './styles';
+
+export default class Conversor extends Component {
+
+    constructor(props) {
+
+        super(props);
+
+        this.state = {
+            moedaA_valor: "",
+            moedaB_valor: 0
+        }
+
+        this.converter = this.converter.bind(this);
+
+    }
+
+    converter(){
+        
+        let de_para = `${this.props.moedaA}_${this.props.moedaB}`;
+
+        let url = `https://free.currconv.com/api/v7/convert?q=${de_para}&compact=ultra&apiKey=9e81814401d2f560f794`;
+
+        fetch(url).then(res => {
+            return res.json();
+        }).then(json => {
+            let cotacao = json[de_para];
+            let moedaB_valor = (parseFloat(this.state.moedaA_valor * cotacao).toFixed());
+            this.setState({moedaB_valor});
+        })
+        
+    }
+
+    render() {
+        return(
+            <div className="conversor">
+                <h2>{this.props.moedaA} para {this.props.moedaB}</h2>
+                <input type="text" onChange={(event) => {this.setState({moedaA_valor: event.target.value})}}></input>
+                <input type="button" value="converter" onClick={this.converter}></input>
+                <h2>{this.state.moedaB_valor}</h2>
+            </div>
+        );
+    }
+}
